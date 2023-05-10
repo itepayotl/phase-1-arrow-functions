8df38d69ac9a224f7d1e3786d610ab53acebcc4b
Israel <itepayotl@yahoo.com>
2023-05-10 13:04:08 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide =(parameter1, parameter2) => parameter1 / parameter2
+divide(2000,100)
+const square = x => x * x
+const add = ( parameter1 , parameter2) => {
+    return parameter1 + parameter2;
+}
